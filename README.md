0e05aee0002f96ab097d56aba6825e73daf9ff74
SAHRIYahyaAbdelkadouss <yahyaabdelkadouss.sahri@usmba.ac.ma>
2022-07-23 21:15:08 +0000

    my first commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # gomyCode-NodeJs
 This repo is made to contain all exirces of the course.
+
+# run the app
+npm install -g learnyounode
+learnyounode
+
---
+++ hello-world/hello-world.js
@@ -0,0 +1,17 @@
+// ## HELLO WORLD (Exercise 1 of 13)  by Yahya Abdelkadouss SAHRI
+
+//   Create a file named hello-world.js.
+//   Write a program that prints the text "HELLO WORLD" to the console
+//   (stdout).
+
+//  ─────────────────────────────────────────────────────────────────────────────
+
+//  ## HINTS
+
+//   To make a Node.js program, create a new file with a .js extension and
+//   start writing JavaScript! Execute your program by running it with the node
+//   command. e.g.:
+
+//      $ node hello-world.js
+
+console.log("HELLO WORLD");
